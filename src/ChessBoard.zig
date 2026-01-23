d998f70353f251fdcf7732ce89a423e40fe99ef8
andrew.kraevskiii <andrew.kraevskii@gmail.com>
2026-01-24 02:02:55 +0300

    Implement Fen parsing.

--- src/ChessBoard.zig
+++ src/ChessBoard.zig
@@ -4,7 +4,6 @@ const ChessBoard = @This();
 
 cells: [8][8]?PieceWithSide,
 turn: Side,
-moves: u16,
 can_castle: std.EnumArray(Side, std.EnumArray(CastleSide, bool)),
 /// Points to square which can be eaten by pawn.
 /// In this situation shows square E where p is pawn
@@ -13,6 +12,7 @@ can_castle: std.EnumArray(Side, std.EnumArray(CastleSide, bool)),
 /// .    p
 en_passant: ?Position,
 halfmove_clock: u32,
+moves: u32,
 
 pub const Move = struct {
     from: Position,
@@ -44,6 +44,82 @@ pub const Move = struct {
 
 pub fn get(board: *ChessBoard, pos: Position) *?PieceWithSide {
     return &board.cells[7 - pos.row][pos.file];
+}
+
+pub fn fromFen(fen_string: []const u8) error{InvalidFen}!ChessBoard {
+    var tokens = std.mem.tokenizeScalar(u8, fen_string, ' ');
+    const board_str = tokens.next() orelse return error.InvalidFen;
+    const turn = tokens.next() orelse return error.InvalidFen;
+    const castle = tokens.next() orelse return error.InvalidFen;
+    const en_passant = tokens.next() orelse return error.InvalidFen;
+    const half_turn = tokens.next() orelse return error.InvalidFen;
+    const full_turn = tokens.next() orelse return error.InvalidFen;
+    if (tokens.next() != null) return error.InvalidFen;
+
+    // fen string can't contain 0s in board part. So it is explicity excluded.
+    if (std.mem.findNone(u8, board_str, "rnbqkbnrRNBQKBNRpP12345678/")) |pos| {
+        std.log.err("Contains invalid character: {c}", .{board_str[pos]});
+        return error.InvalidFen;
+    }
+    var board: ChessBoard = undefined;
+
+    {
+        var rows_strings = std.mem.tokenizeScalar(u8, board_str, '/');
+        for (&board.cells) |*row| {
+            const row_string = rows_strings.next() orelse return error.InvalidFen;
+            @memset(row, null);
+            var row_pos: u4 = 0;
+            for (row_string) |char| {
+                if ('1' <= char and char <= '8') {
+                    row_pos += @intCast(char - '1');
+                    row_pos += 1;
+                    continue;
+                }
+                if (row_pos >= row.len) return error.InvalidFen;
+                row[row_pos] = .fromChar(char);
+                row_pos += 1;
+            }
+        }
+        if (rows_strings.next() != null) return error.InvalidFen;
+    }
+
+    std.debug.assert(turn.len > 0); // tokenize always returns non empty strings.
+
+    if (turn.len != 1) return error.InvalidFen;
+    board.turn = Side.fromChar(turn[0]) orelse return error.InvalidFen;
+    board.can_castle = .init(.{
+        .white = .init(.{
+            .queen = std.mem.findScalar(u8, castle, 'Q') != null,
+            .king = std.mem.findScalar(u8, castle, 'K') != null,
+        }),
+        .black = .init(.{
+            .queen = std.mem.findScalar(u8, castle, 'q') != null,
+            .king = std.mem.findScalar(u8, castle, 'k') != null,
+        }),
+    });
+
+    if (std.mem.eql(u8, en_passant, "-")) {
+        board.en_passant = null;
+    } else if (en_passant.len == 2) {
+        board.en_passant = Position.fromStringFalible(en_passant[0..2].*) catch return error.InvalidFen;
+    } else {
+        return error.InvalidFen;
+    }
+
+    board.halfmove_clock = std.fmt.parseInt(u32, half_turn, 10) catch return error.InvalidFen;
+    board.moves = std.fmt.parseInt(u32, full_turn, 10) catch return error.InvalidFen;
+
+    return board;
+}
+
+test fromFen {
+    const chess_board: ChessBoard = try .fromFen(
+        \\rnbqkbnr/p7/8/8/8/8/PPPPPPPP/RNBQKBNR b Kq h8 10 100
+    );
+    var buffer: [0x1000]u8 = undefined;
+    var bw: std.Io.Writer = .fixed(&buffer);
+    try chess_board.writeFen(&bw);
+    try std.testing.expectEqualSlices(u8, "rnbqkbnr/p7/8/8/8/8/PPPPPPPP/RNBQKBNR b Kq h8 10 100", bw.buffered());
 }
 
 const king_start_position: std.EnumArray(Side, Position) = .init(.{
@@ -156,6 +232,14 @@ pub const Side = enum {
         return switch (side) {
             .white => 'w',
             .black => 'b',
+        };
+    }
+
+    pub fn fromChar(char: u8) ?Side {
+        return switch (char) {
+            'w' => .white,
+            'b' => .black,
+            else => return null,
         };
     }
 
